b5aec2c4749ab11cfd53ff491a49b7d296b9bc12
PratikNarkhede20 <77179340+PratikNarkhede20@users.noreply.github.com>
2021-05-28 16:26:39 -0700

    Update dmaInternalRegistersPkg.sv

--- dmaInternalRegistersPkg.sv
+++ dmaInternalRegistersPkg.sv
@@ -4,25 +4,25 @@ logic [15 : 0] baseAddress        [3 : 0];
 logic [15 : 0] baseWordCount      [3 : 0];
 logic [15 : 0] currentAddress     [3 : 0];
 logic [15 : 0] currentWordCount   [3 : 0];
-logic [15 : 0] cemporaryAddress          ;
+logic [15 : 0] temporaryAddress          ;
 logic [15 : 0] temporaryWordCount        ;
 logic [7  : 0] temporary                 ;
 
-struct packed{logic c0ReachedTC; //1-Channel 0 has reached Terminal Count
-              logic c1ReachedTC; //1-Channel 1 has reached Terminal Count
-              logic c2ReachedTC; //1-Channel 2 has reached Terminal Count
-              logic c3ReachedTC; //1-Channel 3 has reached Terminal Count
-              logic c0Request  ; //1-Channel 0 Request
-              logic c1Request  ; //1-Channel 1 Request
-              logic c2Request  ; //1-Channel 2 Request
-              logic c3Request  ; //1-Channel 3 Request
+struct packed{logic c0ReachedTC; //1 - Channel 0 has reached Terminal Count
+              logic c1ReachedTC; //1 - Channel 1 has reached Terminal Count
+              logic c2ReachedTC; //1 - Channel 2 has reached Terminal Count
+              logic c3ReachedTC; //1 - Channel 3 has reached Terminal Count
+              logic c0Request  ; //1 - Channel 0 Request
+              logic c1Request  ; //1 - Channel 1 Request
+              logic c2Request  ; //1 - Channel 2 Request
+              logic c3Request  ; //1 - Channel 3 Request
              } status;
 
 struct packed{logic memToMem      ; //0 - Memory to Memory disable, 1 - Memory to Memory enable
               logic c0AddressHold ; //0-Channel 0 address hold disabled,  1-Channel 0 address hold enable, X-if MemToMem=0
               logic controller    ; //0-Controller Enable, 1-Controller Disable
               logic timing        ; //0-Normal Timing, 1-Compressed Timing
-              logic priorityType  ; //0-Fixed Priority, 1-Rotating Priority
+              logic priorityType      ; //0-Fixed Priority, 1-Rotating Priority
               logic writeSelection; //0-Late Write Selection, 1-Extended Write Selection, X-if CompressedTiming=1
               logic dreqSense     ; //0-DREQ sense active high, 1-DREQ sense active low
               logic dackSense     ; //0-DACK sense active low, 1-DACK sense active high
@@ -33,7 +33,7 @@ struct packed{logic [1 : 0] channelSelect     ; //00-Channel0, 01-Channel1, 10-C
               logic         autoinitialization; //0-Autoinitialization Disable, 1-Autoinitialization Enable
               logic         addressSelect     ; //0-Address Increment select, 1-Address Drecement select
               logic [1 : 0] modeSelect        ; //00-Demand Mode , 01-Single Mode, 10-Block Mode, 11-Cascade Mode
-             } mode;
+             } mode [3 : 0];
 
 struct packed{logic c0Mask; //0-Clear Channel 0 mask bit, 1-Set Channel 0 mask bit
               logic c1Mask; //0-Clear Channel 0 mask bit, 1-Set Channel 0 mask bit
