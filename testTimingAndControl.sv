d3a2bfb39f704050b2e7d0747db515f98fe35fb6
Tanmay Patil <tpatil@pdx.edu>
2021-06-02 12:21:24 -0700

    Updated testTimingAndControl.sv

--- testTimingAndControl.sv
+++ testTimingAndControl.sv
@@ -2,22 +2,38 @@ module testTimingAndControl();
 
   bit CLK = 0;
   bit RESET;
-  always #5 CLK = ~CLK;
+  //always #5 CLK = ~CLK;
 
-  cpuInterface.timingAndControl TCcpuIf(CLK, RESET);
-  cpuInterface.priorityLogic PLcpuIf(CLK, RESET);
-  dmaInternalRegistersIf.timingAndControl intRegIf(cpuIf.CLK, cpuIf.RESET);
-  dmaInternalSignalsIf.timingAndControl intSigIf(cpuIf.CLK, cpuIf.RESET);
+  cpuInterface cpuIf(CLK, RESET);
+  dmaInternalRegistersIf intRegIf(cpuIf.CLK, cpuIf.RESET);
+  dmaInternalSignalsIf intSigIf(cpuIf.CLK, cpuIf.RESET);
 
-  timingAndControl DUT(TCcpuIf, PLcpuIf, intRegIf, intSigIf);
+  /*cpuInterface.timingAndControl TCcpuIf;//(CLK, RESET);
+  cpuInterface.priorityLogic PLcpuIf;//(CLK, RESET);
+  dmaInternalRegistersIf.timingAndControl intRegIf;//(cpuIf.CLK, cpuIf.RESET);
+  dmaInternalSignalsIf.timingAndControl intSigIf;//(cpuIf.CLK, cpuIf.RESET);*/
+
+  timingAndControl DUT(cpuIf.timingAndControl, cpuIf.priorityLogic, intRegIf.timingAndControl, intSigIf.timingAndControl);
 
   initial
     begin
-      @(negedge TCcpuIf.CLK);
-      TCcpuIf.RESET = 1'b1;
-      PLcpuIf.DREQ = 4'b0001;
-      PLcpuIf.HLDA = 1'b0;
-      TCcpuIf.EOP_N = 1'b0;
+      forever #5 CLK = ~CLK;
+    end
+
+  initial
+    begin
+      $dumpfile("dumps.vcd");
+      //$dumpvars(0,edge_detect);
+      $dumpvars;
+    end
+
+  initial
+    begin
+      @(negedge CLK);
+      RESET = 1'b1;
+      cpuIf.DREQ = 4'b0001;
+      cpuIf.HLDA = 1'b0;
+      //cpuIf.EOP_N = 1'b0;
       intRegIf.modeReg[0].transferType = 2'b00;
       intRegIf.modeReg[1].transferType = 2'b00;
       intRegIf.modeReg[2].transferType = 2'b00;
