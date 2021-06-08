9649a62bb2f60fd5b55d512d5380cf2c771b0dda
PratikNarkhede20 <pratik2@pdx.edu>
2021-06-08 13:18:54 -0700

    Update datapathTB.sv

--- datapathTB.sv
+++ datapathTB.sv
@@ -59,11 +59,29 @@ module datapathTB();
       repeat(2)@(negedge CLK);
       $display("mode reg[2] = %b", intRegIf.modeReg[2]);
 
-       //write upper Address base address register and Current Address Register
+
+       //write upper byte to base address register and Current Address Register
       @(negedge CLK)
       begin
         {intSigIf.programCondition, cpuIf.CS_N, cpuIf.IOR_N, cpuIf.IOW_N, cpuIf.A3, cpuIf.A2, cpuIf.A1, cpuIf.A0} = 8'b10100010;
         cpuIf.DB = 8'b11001100;
+      end
+
+      repeat(2)@(negedge CLK);
+
+      //clear internal flipflop to load lower address byte
+      @(negedge CLK)
+      begin
+        {intSigIf.programCondition, cpuIf.CS_N, cpuIf.IOR_N, cpuIf.IOW_N, cpuIf.A3, cpuIf.A2, cpuIf.A1, cpuIf.A0} = 8'b10101100;
+      end
+
+      repeat(2)@(negedge CLK);
+
+      //write lower byte to base address register and Current Address Register
+      @(negedge CLK)
+      begin
+        {intSigIf.programCondition, cpuIf.CS_N, cpuIf.IOR_N, cpuIf.IOW_N, cpuIf.A3, cpuIf.A2, cpuIf.A1, cpuIf.A0} = 8'b10100010;
+        cpuIf.DB = 8'b10001000;
       end
 
       repeat(2)@(negedge CLK);
