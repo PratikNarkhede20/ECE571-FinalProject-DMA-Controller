7dc7930da14e1b4dbe33f0427a9fe6cd02ea1984
Tanmay Patil <tpatil@pdx.edu>
2021-06-10 18:22:03 -0700

    Cleaned the codes and directory

--- timingAndControlTB.sv
+++ timingAndControlTB.sv
@@ -11,6 +11,7 @@ module testTimingAndControl();
 
   timingAndControl DUT(busIf.timingAndControl, busIf.priorityLogic, intRegIf.timingAndControl, intSigIf.timingAndControl);
 
+//Task for setting program condition
   task setProgramCondition();
     busIf.CS_N = 1'b0;
 
@@ -19,6 +20,7 @@ module testTimingAndControl();
     busIf.CS_N = 1'b1;
   endtask
 
+//Task to request a transaction
   task transactionRequest(
     input logic [3:0] DREQ,
     input logic [7:0] transactionType,
@@ -31,6 +33,7 @@ module testTimingAndControl();
     intRegIf.temporaryAddressReg = addressReg;
   endtask
 
+//Task to do a reset
   task doReset();
     RESET = 1'b1;
 
@@ -40,11 +43,6 @@ module testTimingAndControl();
 
   always @(negedge CLK)
     begin
-      /*if(DUT.state == 6'b100000 && intSigIf.updateCurrentWordCountReg) //TO DO - Add comment here!! CAUTION - When doing block level testing replace "intSigIf.updateCurrentWordCountReg" with "intSigIf.intEOP"
-        begin
-          //busIf.HLDA = 1'b0;
-          busIf.DREQ = 4'b0000;
-        end*/
       if(intSigIf.assertDACK)
         busIf.DACK = busIf.DREQ;
       else
@@ -55,8 +53,6 @@ module testTimingAndControl();
       else
         busIf.HLDA = 1'b0;
 
-      /*if(!busIf.HRQ)
-        busIf.HLDA = 1'b0;*/
       if(intSigIf.decrTemporaryWordCountReg)
         intRegIf.temporaryWordCountReg = intRegIf.temporaryWordCountReg - 1'b1;
 
@@ -67,33 +63,11 @@ module testTimingAndControl();
   always @(posedge intSigIf.intEOP)
   begin
     busIf.DREQ = 4'b0000;
-    //busIf.HLDA = 1'b0;
   end
-
-  /*always @(posedge busIf.HRQ)
-    begin
-      repeat(WAITE) @(negedge CLK);
-      busIf.HLDA = 1'b1;
-    end*/
-
-  /*always_comb
-  begin
-    if(intSigIf.intEOP) //TO DO - Add comment here!! CAUTION - When doing block level testing replace "intSigIf.updateCurrentWordCountReg" with "intSigIf.intEOP"
-      begin
-        busIf.HLDA = 1'b0;
-        busIf.DREQ = 4'b0000;
-      end
-  end*/
-
-  /*initial
-    begin
-      forever #5 CLK = ~CLK;
-    end*/
 
   initial
     begin
       $dumpfile("dumps.vcd");
-      //$dumpvars(0,edge_detect);
       $dumpvars;
     end
 
@@ -105,36 +79,18 @@ module testTimingAndControl();
       busIf.CS_N = 1'b1;
 
       transactionRequest(4'b0000, 8'b00000000, 16'b00, 16'b00);
-      /*intRegIf.modeReg[0].transferType = 2'b01;
-      intRegIf.modeReg[1].transferType = 2'b00;
-      intRegIf.modeReg[2].transferType = 2'b00;
-      intRegIf.modeReg[3].transferType = 2'b00;
-      intRegIf.temporaryWordCountReg = 16'b01;*/
 
       @(negedge CLK);
       RESET = 1'b0;
 
       @(negedge CLK);
       setProgramCondition();
-      /*busIf.CS_N = 1'b0;
-
-      WAITE = $urandom_range(7,1);
-      repeat(WAITE) @(negedge CLK);
-      busIf.CS_N = 1'b1;*/
 
       repeat(1) @(negedge CLK);
       transactionRequest(4'b0001, 8'b01000000, 16'b10, 16'b11);
-      //busIf.DREQ = 4'b0001;
 
       repeat(6) @(negedge CLK);
       doReset();
-
-      /*@(negedge CLK);
-      if(intSigIf.intEOP)
-        begin
-          busIf.HLDA = 1'b0;
-          busIf.DREQ = 4'b0000;
-        end*/
 
       #200
       $stop;
